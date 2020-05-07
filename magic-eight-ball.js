373013d33fda6044ebf66a63f73cab72433f83e5
Jed Boyle <jedboyle01@gmail.com>
2020-05-07 13:36:16 +0100

    Create a variable named userQuestion. The value of the variable should be a string that is the question the user wants to ask the Magic Eight Ball.

--- magic-eight-ball.js
+++ magic-eight-ball.js
@@ -2,3 +2,6 @@ let userName = '';
 
 // Create a ternary expression that decides what to do if the user enters a name or not.
 userName ? console.log(`Hello ${userName}`) : console.log(`Hello`);
+
+// Create a variable named userQuestion. The value of the variable should be a string that is the question the user wants to ask the Magic Eight Ball.
+let userQuestion = 'Will I become a wearwolf tonight?';
