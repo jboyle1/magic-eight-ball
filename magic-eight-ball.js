0253a26f57807c3534400eed7f407eaf12069b86
Jed Boyle <jedboyle01@gmail.com>
2020-05-07 13:43:02 +0100

    Write a console.log() for the userQuestion, stating what was asked. You can include the user’s name in the console.log() statement, if you wish! Also generate a random number between 0 and 7 and store it in a variable called randomNumber.

--- magic-eight-ball.js
+++ magic-eight-ball.js
@@ -5,3 +5,9 @@ userName ? console.log(`Hello ${userName}`) : console.log(`Hello`);
 
 // Create a variable named userQuestion. The value of the variable should be a string that is the question the user wants to ask the Magic Eight Ball.
 let userQuestion = 'Will I become a wearwolf tonight?';
+
+// Write a console.log() for the userQuestion, stating what was asked. You can include the user’s name in the console.log() statement, if you wish!
+console.log(`${userName} asked: ${userQuestion}`);
+
+// Generate a random number between 0 and 7. Create another variable, and name it randomNumber. Set it equal to this expression, which uses two methods from the Math library.
+let randomNumber = Math.floor(Math.random() * 8);
